7afd732571369906ed08dc5c94520e874fe54df5
djena <djena@beaconstreetservices.com>
2021-07-04 10:07:59 -0400

    Implementing text_to_speech.. partially complete

--- lib/ui/script.dart
+++ lib/ui/script.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:highlight_text/highlight_text.dart';
 
+import '/service/text_to_speech.dart';
+import '/util/util.dart';
 import 'load.dart';
 import 'menudrawer.dart';
 
@@ -7,6 +10,8 @@ import 'menudrawer.dart';
 class Script extends StatelessWidget {
   ///Log
   final String log;
+
+  final _tts = TextToSpeech();
 
   ///Script to read
   Script({Key key, @required this.log}) : super(key: key);
@@ -18,12 +23,19 @@ class Script extends StatelessWidget {
           Icons.view_headline_outlined,
           size: 40,
         ),
-        title: Text("Read a Note"),
+        title: Text("Note"),
       ),
       endDrawer: MenuDrawer(),
       body: Column(
         children: [
-          Text(log),
+          TextHighlight(
+            text: log,
+            words: highlights,
+            textStyle: const TextStyle(
+                fontSize: 32.0,
+                color: Colors.black,
+                fontWeight: FontWeight.w400),
+          ),
           ElevatedButton(
             onPressed: () {
               Navigator.push(
@@ -34,6 +46,65 @@ class Script extends StatelessWidget {
             child: Text("Back"),
           ),
         ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          margin: EdgeInsets.only(left: 12.0, right: 12.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                //update the bottom app bar view each time an item is clicked
+                onPressed: () {
+                  _tts.speak(log);
+                },
+                iconSize: 27.0,
+                icon: Icon(
+                  Icons.volume_up,
+                  color: Colors.blue.shade900,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  //updateTabSelection(1, "Outgoing");
+                },
+                iconSize: 27.0,
+                icon: Icon(
+                  Icons.call_made,
+                  color: Colors.blue.shade900,
+                ),
+              ),
+              SizedBox(
+                width: 50.0,
+              ),
+              IconButton(
+                onPressed: () {
+                  // updateTabSelection(2, "Incoming");
+                },
+                iconSize: 27.0,
+                icon: Icon(
+                  Icons.call_received,
+                  color: Colors.blue.shade900,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  // updateTabSelection(3, "Settings");
+                },
+                iconSize: 27.0,
+                icon: Icon(
+                  Icons.settings,
+                  color: Colors.blue.shade900,
+                ),
+              ),
+            ],
+          ),
+        ),
+        //to add a space between the FAB and BottomAppBar
+        shape: CircularNotchedRectangle(),
+        //color of the BottomAppBar
+        color: Colors.white,
       ),
     );
   }
