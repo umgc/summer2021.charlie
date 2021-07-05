c2b4ab33885ed257bbffc4ac3cfd776cdaa8c70e
dkimbi <84943702+dkimbi@users.noreply.github.com>
2021-07-04 20:17:05 -0400

    Adding schedule delete

--- lib/service/scheduled_delete_text.dart
+++ lib/service/scheduled_delete_text.dart
@@ -1,5 +1,6 @@
 import 'package:mnemosyne/ui/textmap.dart';
 
+
 class ScheduledDeleteText {
   TextMap logs = new TextMap();
   void deleteText (int numberDays) async {
@@ -7,19 +8,34 @@ class ScheduledDeleteText {
     String fileText = await logs.getDecryptedContent();
     Map dateTimeText = logs.readJson(fileText);
 
+
+
+    //dateTimeText.removeWhere((key, value) => false)
+    dateTimeText.removeWhere((key, value) {
+//Date dt = convertStringToDate(key);
+      DateTime dt = DateTime.parse(key);
+//return dt < now() - numberOfDays;
+      return dt.isBefore(DateTime.now().subtract(new Duration(days: 7)));
+    });
+
+
+    /*
     //dateTimeText.removeWhere((key, value) => false)
     dateTimeText.removeWhere((key, value) {
       //Date dt = convertStringToDate(key);
-      Date dt = DateTime.parse(key);
+      var dt = DateTime.parse(key);
       //return dt < now() - numberOfDays;
       return dt < DateTime.now().subtract(new Duration(days: 7));
     });
+    */
 
   }
 
+/*
   var encryptedBase64 = _encryptionService.encrypt(toJson(dateTimeText));
   file.writeAsString(encryptedBase64);
-  
+*/
+
 }
 
 
