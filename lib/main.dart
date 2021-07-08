32ba15779e4cbc0d291238ce56c1d7e358b82d10
dkimbi <84943702+dkimbi@users.noreply.github.com>
2021-07-08 19:49:59 -0400

    Flag exception on isFavorite for delete jobs & code refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:workmanager/workmanager.dart';
+
+import 'service/scheduled_delete_text.dart';
 import 'ui/audio_recognize.dart';
-// import 'package:workmanager/workmanager.dart';
 
-/* const myTask = "syncWithTheBackEnd";
+/// This task runs periodically
+const simplePeriodic1HourTask = "Old Notes Deleted";
 
+/// callback dispatcher
 void callbackDispatcher() {
-// this method will be called every hour
-  Workmanager.executeTask((task, inputdata) async {
-    switch (task) {
-      case myTask:
-        print("this method was called from native!");
-        Fluttertoast.showToast(msg: "this method was called from native!");
-        break;
+  Workmanager().executeTask((task, inputData) async {
+    var scheduledDeleteText = ScheduledDeleteText();
+    scheduledDeleteText.deleteText(7);
+    print("$simplePeriodic1HourTask was executed");
 
-      case Workmanager.iOSBackgroundTask:
-        print("iOS background fetch delegate ran");
-        break;
-    }
-
-    //Return true when the task executed successfully or not
+    ///Return true when the task executed successfully or not
     return Future.value(true);
   });
 }
- */
+
 void main() {
-// needs to be initialized before using workmanager package
-  /* WidgetsFlutterBinding.ensureInitialized();
+  ///Initializing work manager
+  WidgetsFlutterBinding.ensureInitialized();
 
-  // initialize Workmanager with the function which you want to invoke after any periodic time
-  Workmanager.initialize(callbackDispatcher);
-
-  // Periodic task registration
-  Workmanager.registerPeriodicTask(
-    "2",
-    // use the same task name used in callbackDispatcher function for identifying the task
-    // Each task must have a unique name if you want to add multiple tasks;
-    myTask,
-    // When no frequency is provided the default 15 minutes is set.
-    // Minimum frequency is 15 min.
-    // Android will automatically change your frequency to 15 min if you have configured a lower frequency than 15 minutes.
-    frequency: Duration(minutes: 15), // change duration according to your needs
+  ///void initialize() {
+  Workmanager().initialize(
+    callbackDispatcher,
+    isInDebugMode: true,
   );
- */
+  Workmanager().registerPeriodicTask(
+    "6",
+    simplePeriodic1HourTask,
+    initialDelay: Duration(seconds: 10),
+    frequency: Duration(hours: 24),
+  );
+
   runApp(MyApp());
 }
 
--- lib/service/scheduled_delete_text.dart
+++ lib/service/scheduled_delete_text.dart
@@ -1,4 +1,4 @@
-import 'package:workmanager/workmanager.dart';
+import 'dart:convert';
 
 import '../ui/textmap.dart';
 
@@ -7,39 +7,20 @@ class ScheduledDeleteText {
   /// logs
   final TextMap logs = TextMap();
 
-  void _deleteText(int numberDays) async {
+  /// delete
+  void deleteText(int numberDays) async {
     var fileText = await logs.getDecryptedContent();
     var dateTimeText = logs.readJson(fileText);
 
     dateTimeText.removeWhere((key, value) {
       var dt = DateTime.parse(key);
       //todo add isFavorites exception for deletes
-      return dt.isBefore(DateTime.now().subtract(Duration(days: numberDays)));
+      var data = json.decode(value);
+      return dt.isBefore(DateTime.now().subtract(Duration(days: numberDays))) &&
+          data['isFavorites'] == false;
     });
     //Write to file after adding log
     logs.writeFile(dateTimeText);
   }
 }
 
-/// callback dispatcher
-void callbackDispatcher() {
-  Workmanager().executeTask((task, inputData) async {
-    var scheduledDeleteText = ScheduledDeleteText();
-    scheduledDeleteText._deleteText(7);
-    return Future.value(true);
-  });
-}
-
-/// initialising work manager
-void initialize() {
-  Workmanager().initialize(
-    callbackDispatcher,
-    isInDebugMode: true,
-  );
-  Workmanager().registerPeriodicTask(
-    "6",
-    "Periodic1hourTask",
-    initialDelay: Duration(seconds: 10),
-    frequency: Duration(minutes: 15),
-  );
-}
